220d80ed0a883cbf78af53b18b87a81cb1bae81c
kristanto08 <113617653+kristanto08@users.noreply.github.com>
2022-10-26 23:12:36 +0700

    Create MovieList.js

---
+++ src/containers/MovieList.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react"
+import "./MovieList.css"
+import { useParams } from "react-router-dom"
+import Cards from "../components/MovieCard"
+
+const MovieList = () => {
+    
+    const [movieList, setMovieList] = useState([])
+    const {type} = useParams()
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    useEffect(() => {
+        getData()
+    }, [type])
+
+    const getData = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=525094b34f3293579a503cfa67bf7a05&language=en-US`)
+        .then(res => res.json())
+        .then(data => setMovieList(data.results))
+    }
+
+    return (
+        <div className="movie__list">
+            <h2 className="list__title">{(type ? type : "BEST-MOVIES").toUpperCase()}</h2>
+            <div className="list__cards">
+                {
+                    movieList.map(movie => (
+                        <Cards movie={movie} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MovieList
