41484ed595c87bda3ca27774ee898e0af475df57
kristanto08 <113617653+kristanto08@users.noreply.github.com>
2022-10-26 22:05:44 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Login from './containers/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+import Register from './containers/Register';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={
+          <ProtectedRoute>
+            <App />
+          </ProtectedRoute>} />
+          <Route path="private" element={
+          <ProtectedRoute>
+            <div>ini rahasia</div>
+          </ProtectedRoute>} />
+        <Route path="login" element={
+          <ProtectedRoute loginOnly={false}>
+            <Login />
+          </ProtectedRoute>} />
+        <Route path="register" element={
+          <ProtectedRoute loginOnly={false}>
+            <Register />
+          </ProtectedRoute>} />
+          </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
